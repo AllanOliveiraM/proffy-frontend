88b3c91e81a3b47d5aea69eba48f3c10513ad821
AllanOliveiraM <nallamo2016@gmail.com>
2020-08-11 18:20:34 -0300

    Finished TeacherForm Page with API Connection

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, FormEvent } from "react";
+import { useHistory } from "react-router-dom";
 
 import PageHeader from "../../components/PageHeader";
 import Input from "../../components/Input";
@@ -14,6 +15,7 @@ import lang from "../../assets/lang/pt-br/index.json";
 import "./styles.css";
 
 function TeacherForm() {
+  const history = useHistory();
   const [name, setName] = useState("");
   const [avatar, setAvatar] = useState("");
   const [whatsapp, setWhatsapp] = useState("");
@@ -61,6 +63,8 @@ function TeacherForm() {
       })
       .then(() => {
         alert("Cadastro realizado com sucesso!");
+
+        history.push("/");
       })
       .catch(() => {
         alert("Erro no cadastro!");
